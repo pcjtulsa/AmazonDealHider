017ca9a1c0cb395f15c301c2935feb84470692d4
pcj <pcj@pcj.us>
2015-08-02 18:18:08 -0500

    add the other sites

--- amazondeals.js
+++ amazondeals.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2015 Patrick Johnston. All rights reserved.
-
-var hideThis = chrome.contextMenus.create({"title": "Show/Hide this deal", "contexts": ["link"], "onclick":showHideDeal, "documentUrlPatterns": ["*://*.amazon.com/*","*://*.amazon.co.uk/*"]});
-
+var urlPatterns = ["*://*.amazon.com/*","*://*.amazon.ca/*","*://*.amazon.co.uk/*","*://*.amazon.co.jp/*","*://*.amazon.de/*","*://*.amazon.fr/*"];
+var hideThis = chrome.contextMenus.create({"title": "Show/Hide this deal", "contexts": ["link"], "onclick":showHideDeal, "documentUrlPatterns": urlPatterns});
 function showHideDeal(info,tab) {
 	var ASINpatt = /.*?\.?ama?zo?n\.(?:com|ca|co\.uk|co\.jp|de|fr)\/(?:exec\/obidos\/ASIN\/|o\/|gp\/product\/|(?:(?:[^"\'\/]*)\/)?dp\/|)(B[A-Z0-9]{9})(?:(?:\/|\?|\#)(?:[^"\'\s]*))?/
 	var ASIN = info.linkUrl.replace(ASINpatt,"$1");
@@ -33,7 +32,7 @@ function showHideDeal(info,tab) {
 	});
 }
 
-//var showAll = chrome.contextMenus.create({"title": "Show all hidden deals", "contexts": ["page","link","image"], "type": "checkbox", "onclick":showAllClicked, "documentUrlPatterns": ["*://*.amazon.com/*","*://*.amazon.co.uk/*"]});
+//var showAll = chrome.contextMenus.create({"title": "Show all hidden deals", "contexts": ["page","link","image"], "type": "checkbox", "onclick":showAllClicked, "documentUrlPatterns": urlPatterns});
 
 function showAllClicked(info, tab) {
 	chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
@@ -41,7 +40,7 @@ function showAllClicked(info, tab) {
 	});
 }
 
-var clearList = chrome.contextMenus.create({"title": "Clear list of deals to hide", "contexts": ["page","link","image"], "onclick":clearASINs, "documentUrlPatterns": ["*://*.amazon.com/*","*://*.amazon.co.uk/*"]});
+var clearList = chrome.contextMenus.create({"title": "Clear list of deals to hide", "contexts": ["page","link","image"], "onclick":clearASINs, "documentUrlPatterns": urlPatterns});
 
 function clearASINs(info,tab) {
 	if (!confirm("Are you sure you want to clear the list?")) return;
