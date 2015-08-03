87df41a2ee864eab505ddc4d9848396f3e2275ea
pcj <pcj@pcj.us>
2015-08-02 22:01:02 -0500

    work around storage size limit

--- amazondeals.js
+++ amazondeals.js
@@ -1,11 +1,15 @@
 // Copyright (c) 2015 Patrick Johnston. All rights reserved.
 var urlPatterns = ["*://*.amazon.com/*","*://*.amazon.ca/*","*://*.amazon.co.uk/*","*://*.amazon.co.jp/*","*://*.amazon.de/*","*://*.amazon.fr/*"];
 var hideThis = chrome.contextMenus.create({"title": "Show/Hide this deal", "contexts": ["link"], "onclick":showHideDeal, "documentUrlPatterns": urlPatterns});
+function getCacheKey(key, i) {
+	return (i === 0) ? key : key + "_" + i;
+}
+
 function showHideDeal(info,tab) {
 	var ASINpatt = /.*?\.?ama?zo?n\.(?:com|ca|co\.uk|co\.jp|de|fr)\/(?:exec\/obidos\/ASIN\/|o\/|gp\/product\/|(?:(?:[^"\'\/]*)\/)?dp\/|)(B[A-Z0-9]{9})(?:(?:\/|\?|\#)(?:[^"\'\s]*))?/
 	var ASIN = info.linkUrl.replace(ASINpatt,"$1");
 	if (!ASIN || ASIN.indexOf("http") != -1) {
-		var dealIDpatt = /.*?\.?ama?zo?n\.(?:com|ca|co\.uk|co\.jp|de|fr)\/(?:s|b)(?:\/browse|)\/ref=.*?([0-9a-f]{8})\?.*/
+		var dealIDpatt = /.*?\.?ama?zo?n\.(?:com|ca|co\.uk|co\.jp|de|fr)(?:\/jewelry|)\/(?:s|b|gp\/search|gp\/coupon.*?)(?:\/browse|)\/ref=.*?([0-9a-f]{8})\?.*/
 		ASIN = info.linkUrl.replace(dealIDpatt,"$1");
 		if (!ASIN || ASIN.indexOf("http") != -1) {
 			console.log("Error: ASIN not found.");
@@ -13,22 +17,40 @@ function showHideDeal(info,tab) {
 		}
 		console.log("Using deal ID.");
 	}
-	chrome.storage.sync.get("blockASINs",function (items) {
-		if (items.blockASINs == undefined) items.blockASINs = [];
-		var ASINlist = items.blockASINs;
+	chrome.storage.sync.get(null,function (items) {
+		var i = 0;
+		var value = "";
+		for(i=0; i<chrome.storage.sync.MAX_ITEMS; i++) {
+			if(items[getCacheKey("blockASINs", i)] === undefined) break;
+			value += items[getCacheKey("blockASINs", i)];
+		}
+		var ASINlist = JSON.parse(value);
 		if (ASINlist.indexOf(ASIN) != -1) {
-			console.log("Show ASIN "+ASIN);
+			console.log("Show "+ASIN);
 			ASINlist.splice(ASINlist.indexOf(ASIN),1);
 		}
 		else {
-			console.log("Hide ASIN "+ASIN);
+			console.log("Hide "+ASIN);
 			ASINlist.push(ASIN);
 		}
-		chrome.storage.sync.set({"blockASINs":ASINlist},function() {
+		i = 0;
+		var cache = {};
+		var segment;
+		var cacheKey;
+		value = JSON.stringify(ASINlist);
+		while(value.length > 0) {
+			cacheKey = getCacheKey("blockASINs", i);
+			segment = value.substr(0, chrome.storage.sync.QUOTA_BYTES_PER_ITEM - cacheKey.length - 2);
+			cache[cacheKey] = segment;
+			value = value.substr(chrome.storage.sync.QUOTA_BYTES_PER_ITEM - cacheKey.length - 2);
+			i++;
+		}
+		chrome.storage.sync.set({"blockASINs":cache},function() {
 			chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
 				chrome.tabs.sendMessage(tabs[0].id,{command:"RefreshDeals"});
 			});
 		});
+		chrome.storage.sync.remove(getCacheKey("blockASINs", i));
 	});
 }
 var sep = chrome.contextMenus.create({"type":"separator","contexts":["link"],"documentUrlPatterns":urlPatterns});
@@ -44,7 +66,7 @@ var clearList = chrome.contextMenus.create({"title": "Clear list of deals to hid
 
 function clearASINs(info,tab) {
 	if (!confirm("Are you sure you want to clear the list?")) return;
-	chrome.storage.sync.set({"blockASINs":[]},function() {
+	chrome.storage.sync.clear(function() {
 		console.log("Clearing list");
 		chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
 			chrome.tabs.sendMessage(tabs[0].id,{command:"RefreshDeals"});
@@ -53,8 +75,14 @@ function clearASINs(info,tab) {
 }
 
 chrome.runtime.onMessageExternal.addListener(function (request, sender, sendResponse) {
-	chrome.storage.sync.get("blockASINs",function (items) {
-		sendResponse({list: items.blockASINs});
+	chrome.storage.sync.get(null,function (items) {
+		var i = 0;
+		var value = "";
+		for(i=0; i<chrome.storage.sync.MAX_ITEMS; i++) {
+			if(items[getCacheKey("blockASINs", i)] === undefined) break;
+			value += items[getCacheKey("blockASINs", i)];
+		}
+		sendResponse({list: JSON.parse(value)});
 	});	
 	return true;
 });
