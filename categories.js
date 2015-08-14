55626fab70e92aa121f8799410c349686ab6797a
pcj <pcj@pcj.us>
2015-08-13 21:18:33 -0500

    testing categories

---
+++ categories.js
@@ -0,0 +1,10 @@
+jQuery("a.a-declarative").mouseover(function() {
+	jQuery("ul.gbw_pop_ul").children().each(function() {
+		jQuery(this).children("a").css("width","135px");
+		if (jQuery(this).html().indexOf("catHider")==-1) jQuery(this).prepend("<span style='float:right;'><input type='checkbox' class='catHider' /></span>");
+	});
+	jQuery("input.catHider").click(function(){
+		var v = jQuery(this).parent().next().attr("value");
+		alert(v + " checked: "+jQuery(this).prop("checked"));
+	});
+});
