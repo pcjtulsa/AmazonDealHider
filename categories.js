0c0f7c7470b82013f10c5f6f3d199f2e34e8cd96
pcj <pcj@pcj.us>
2015-08-13 22:02:29 -0500

    cleanup

--- categories.js
+++ categories.js
@@ -1,12 +1,25 @@
 jQuery("body").delegate("input.catHider","click",function(){
-	var v = jQuery(this).parent().next().attr("value");
-	alert(v + " checked: "+jQuery(this).prop("checked"));	
+	var v = jQuery(this).val();
+	var c = jQuery(this).prop("checked");
+	var $sp = jQuery(this).parent().parent().parent().parent().parent();
+	if (v=="all") {
+		$sp.find("input").prop("checked",c);
+	}
+	else {
+		var $a = $sp.find('input[value="all"]');
+		if (!c) $a.prop("checked",false);
+		else {
+			if ($sp.find("input").not('input[value="all"]').length == $sp.find("input:checked").not('input[value="all"]').length) $a.prop("checked",true);
+			else $a.prop("checked",false);
+		}
+	}
+	alert(v + " checked: "+c);	
 });
 jQuery("a.a-declarative").mouseover(function() {
 	jQuery("ul.gbw_pop_ul").children().each(function() {
 		jQuery(this).children("a").css("width","135px");
-		if (jQuery(this).html()==undefined) return;
-		if (jQuery(this).html().indexOf("catHider")==-1) jQuery(this).prepend("<span style='float:right;'><input type='checkbox' class='catHider' /></span>");
+		if (jQuery(this).html()==undefined || jQuery(this).hasClass("has_divider")) return;
+		if (jQuery(this).html().indexOf("catHider")==-1) jQuery(this).prepend("<span style='float:right;'><input value='"+jQuery(this).find("a").attr("value")+"' type='checkbox' class='catHider' /></span>");
 	});
 
 });
