a510b907ca84946be7edc192594de50570ab60c3
pcj <pcj@pcj.us>
2015-08-13 21:42:20 -0500

    tweaks

--- categories.js
+++ categories.js
@@ -1,10 +1,12 @@
+jQuery("body").delegate("input.catHider","click",function(){
+	var v = jQuery(this).parent().next().attr("value");
+	alert(v + " checked: "+jQuery(this).prop("checked"));	
+});
 jQuery("a.a-declarative").mouseover(function() {
 	jQuery("ul.gbw_pop_ul").children().each(function() {
 		jQuery(this).children("a").css("width","135px");
+		if (jQuery(this).html()==undefined) return;
 		if (jQuery(this).html().indexOf("catHider")==-1) jQuery(this).prepend("<span style='float:right;'><input type='checkbox' class='catHider' /></span>");
 	});
-	jQuery("input.catHider").click(function(){
-		var v = jQuery(this).parent().next().attr("value");
-		alert(v + " checked: "+jQuery(this).prop("checked"));
-	});
+
 });
